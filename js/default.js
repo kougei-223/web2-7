b78af27b4e56fe00464cf97eea94b1677a57c51c
kougei-223 <38421192+kougei-223@users.noreply.github.com>
2018-11-12 18:04:10 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -5,9 +5,9 @@ var answer = parseInt(window.prompt('数あてゲーム。0~2の数を入力し�
 var message;
 if(answer === number){
   message = 'あたり！';
-}else if(answer < number){
+}else if (answer < number){
   message = '残念！もっと大きいです';
-}else if(number < answer){
+}else if (number < answer){
   message = '残念！もっと小さいです';
 }else{
   message = '0~2の数字を入力してください';
