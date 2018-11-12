a406cb9baa53a965e42f58ae28ea557ded0b33f4
kougei-223 <38421192+kougei-223@users.noreply.github.com>
2018-11-12 17:36:15 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,17 @@
+//答えを用意
+var number = Math.floor(Math.ramdom()*3)
+var answer = parseInt(window.prompt('数あてゲー。0~2の数を入力してください！'));
 
+var message;
+if(answer === number){
+  message = 'あたり！';
+}else if(answer < number){
+  message = '残念！もっと大きいです';
+}else if(number < answer){
+  message = '残念！もっと小さいです';
+}else{
+  message = '0~2の数字を入力してください';
+}
+
+//結果を表示
+document.getElementById('choice').textComent = message;
